c3ce63bfc52b3021242087fd9e69598845404427
blackranja <josphatranja243@gmail.com>
2023-10-11 14:34:06 +0300

    Initial Setup

---
+++ src/navigation/index.js
@@ -0,0 +1,17 @@
+import * React from 'react';
+
+import { NativeScreenNavigationContainer, NavigationContainer } from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';  
+import HomeScreen from '../screens/HomeScreen';
+//import { App } from 'expo-router/_app';
+const Stack=createNativeStackNavigator();
+const AppNavigator=()=>{
+    return(
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName='Home' screenOptions={{headerShown:false}}>
+                <Stack.Screen name="Home" component={HomeScreen}/>
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
+export default AppNavigator;
